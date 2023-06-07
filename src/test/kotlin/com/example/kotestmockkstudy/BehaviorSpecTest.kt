50044a77473eebca1d6ecdcc470c2376c98f1308
antaeho <an981022@naver.com>
2023-06-07 15:45:16 +0900

    test: BehaviorSpecTest

---
+++ src/test/kotlin/com/example/kotestmockkstudy/BehaviorSpecTest.kt
@@ -0,0 +1,38 @@
+package com.example.kotestmockkstudy
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class BehaviorSpecTest : BehaviorSpec() {
+
+    init {
+        var str: String
+        var formatter: DateTimeFormatter
+        var actual: LocalDateTime
+
+        given("str is datetime string") {
+            str = "2011-12-03T10:15:30"
+            `when`("parse LocalDateTime form string") {
+                actual = LocalDateTime.parse(str)
+                then("actual should be LocalDateTime object") {
+                    actual shouldBe LocalDateTime.of(2011, 12,3,10,15,30)
+                }
+            }
+        }
+
+        given("str is custom datetime string") {
+            str = "20111203101530"
+            and("formatter is ISO datetime") {
+                formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+                `when`("parse LocalDateTime form string and formatter") {
+                    actual = LocalDateTime.parse(str, formatter)
+                    then("actual should be LocalDateTime object") {
+                        actual shouldBe LocalDateTime.of(2011, 12, 3, 10, 15, 30)
+                    }
+                }
+            }
+        }
+    }
+}
