53d7da93742a2641e4a74f453ddde954cd0e31c1
antaeho <an981022@naver.com>
2023-06-07 14:12:23 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotestmockkstudy/KotestMockkStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotestmockkstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotestMockkStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotestMockkStudyApplication>(*args)
+}
